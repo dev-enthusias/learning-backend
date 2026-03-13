5e4e27dd8bf94c2dbe7dab60e0e7d475e973f7b6
Anakor Joshua <becomingadev@gmail.com>
2026-03-13 15:58:09 +0100

    create script that runs all seed files

---
+++ src/db/seed.ts
@@ -0,0 +1,27 @@
+import db from "db";
+import fs from "node:fs";
+import { fileURLToPath } from "node:url";
+import path, { dirname } from "node:path";
+
+async function seed() {
+  // Locate seeds dir
+  const seedsDir = path.join(dirname(fileURLToPath(import.meta.url)), "seeds");
+
+  // Get all files in the seeds dir
+  const seedFiles = fs.readdirSync(seedsDir);
+  // Read the content of each file and execute it against the database
+  for (const file of seedFiles) {
+    const sql = fs.readFileSync(path.join(seedsDir, file), "utf-8");
+
+    await db.any(sql);
+
+    console.log(`Seed file ${file} executed successfully`);
+  }
+
+  console.log("All seed files executed successfully");
+
+  // exit the process
+  process.exit(0);
+}
+
+seed();
--- src/db/seeds/1773412915042-seed-users.sql
+++ src/db/seeds/1773412915042-seed-users.sql
@@ -1,1 +1,5 @@
--- Write your seed here
+INSERT INTO users (first_name, last_name, email, city) VALUES
+ ('John', 'Doe', 'john.doe@example.com', 'New York'),
+ ('Jane', 'Smith', 'jane.smith@example.com', 'London'),
+ ('Alice', 'Johnson', 'alice.johnson@example.com', 'Paris'),
+ ('Bob', 'Brown', 'bob.brown@example.com', 'Tokyo');
