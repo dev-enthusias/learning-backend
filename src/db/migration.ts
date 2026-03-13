4ada38a7e1f6e68d9d7370be91bdbe6baba5ea9d
Anakor Joshua <becomingadev@gmail.com>
2026-03-13 11:04:09 +0100

    create a migration script that runs all my migration files

--- src/db/migration.ts
+++ src/db/migration.ts
@@ -1,12 +1,27 @@
-import path, { dirname } from "path";
+import fs from "fs";
 import { fileURLToPath } from "url";
+import path, { dirname } from "path";
+import db from "db";
 
-// locate the directory where migrations scripts are
-const migrationsDir = path.join(
-  dirname(fileURLToPath(import.meta.url)),
-  "migrations",
-);
+async function migrate() {
+  // Locate migrations dir
+  const migrationsDir = path.join(
+    dirname(fileURLToPath(import.meta.url)),
+    "migrations",
+  );
 
-// read the directory
+  // Gt all files in the migrations dir
+  const files = fs.readdirSync(migrationsDir);
 
-// read the files and run them all
+  // Read the content of each file and execute it against the database
+  for (const file of files) {
+    const sql = fs.readFileSync(path.join(migrationsDir, file), "utf-8");
+    console.log(sql)
+
+    await db.any(sql);
+  }
+
+  process.exit(0);
+}
+
+migrate();
