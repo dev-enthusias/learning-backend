adb6cc2c3c622279610d6fae26936d17a98fcc35
Anakor Joshua <becomingadev@gmail.com>
2026-03-14 12:40:30 +0100

    chore: add Prettier configuration and update dependencies; i

--- src/db/seeds/0001-seed-users.sql
+++ src/db/seeds/0001-seed-users.sql
@@ -1,5 +1,7 @@
-INSERT INTO users(first_name, last_name, email,city) VALUES
-(Ana, Kor, 5K0Hq@example.com, Lagos),
-(Pamela, Korish, 5K0Hq@example.com, Lagos),
-(Romania, Kor, 5K0Hq@example.com, Lagos),
-(Southern, Smart, 5K0Hq@example.com, Lagos);
+INSERT INTO
+    users(first_name, last_name, email, city)
+VALUES
+    (Ana, Kor, 5K0Hq @example.com, Lagos),
+    (Pamela, Korish, 5K0Hq @example.com, Lagos),
+    (Romania, Kor, 5K0Hq @example.com, Lagos),
+    (Southern, Smart, 5K0Hq @example.com, Lagos);
--- src/modules/users/user.repository.ts
+++ src/modules/users/user.repository.ts
@@ -1,16 +1,23 @@
 import db from "db";
 
 export const findAllUsers = async () => {
-  return await db.any(
-    `SELECT 
-        id user_id,
-        last_name || ', ' || first_name display_name, 
-        first_name || ' ' || last_name || ' <' || email || '>' contact, 
-        city location, 
-        'Joined: ' || EXTRACT(YEAR FROM created_at) member_since 
-      FROM users 
-      ORDER BY 
-        last_name, 
-        first_name DESC;`,
-  );
+    const query = /*sql*/ `
+        SELECT
+            id user_id,
+            last_name || ', ' || first_name display_name,
+            first_name || ' ' || last_name || ' <' || email || '>' contact,
+            city location,
+            'Joined: ' || EXTRACT(
+                YEAR
+                FROM
+                    created_at
+            ) member_since
+        FROM
+            users
+        ORDER BY
+            last_name,
+            first_name DESC;
+    `;
+
+    return await db.any(query);
 };
